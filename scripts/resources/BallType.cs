29c3fddcb8b92f1a0866700a6615fd6b1d056ce9
kifo99 <ivan123rakovicgmail.com>
2025-08-07 19:10:44 +0200

    refactor: polish bouncing physics and add BallType resource

---
+++ scripts/resources/BallType.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+namespace Game.Physics
+{
+
+  [GlobalClass]
+  public partial class BallType : Resource
+  {
+    [Export] public string Name { get; set; } = "Default";
+    [Export] public float Mass { get; set; } = 1f;
+    [Export] public float Restitution { get; set; } = 0.5f;
+    [Export] public float Friction { get; set; } = 0.3f;
+    [Export] public float MaxSpeed { get; set; } = 400f;
+    [Export] public float JumpForce { get; set; } = -500f;
+    [Export] public float MoveForce { get; set; } = 400f;
+  
+
+  }
+}
